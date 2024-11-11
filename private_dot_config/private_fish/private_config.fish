0d32b45443be8add6b3a0d8fe5d45d7ecad978cc
Mikey O'Brien <me@mikeyobrien.com>
2024-11-11 10:25:04 -0600

    Add common fish aliases

--- private_dot_config/private_fish/private_config.fish
+++ private_dot_config/private_fish/private_config.fish
@@ -3,8 +3,39 @@ if status is-interactive
 end
 
 
-alias cmdiff='chezmoi diff'
+# git functions
+alias ga='git add'
+alias gaa='git add --all'
+alias gb='git branch'
+alias gc='git commit'
+alias gcm='git commit -m'
+alias gco='git checkout'
+alias gd='git diff'
+alias gl='git log'
+alias gp='git push'
+alias gpl='git pull'
+alias gs='git status'
+alias gst='git stash'
+alias gsw='git switch'
+alias grb='git rebase'
+alias grs='git reset'
+alias gm='git merge'
+alias gf='git fetch'
+alias gcl='git clone'
+alias grm='git remote'
+alias grv='git remote -v'
+alias glog='git log --oneline --decorate --graph'
+alias gnuke='git reset --hard && git clean -fd'
 
-function cmdiff
-	chezmoi diff
+
+alias cmdiff='chezmoi diff'
+alias cmcd='cd $(chezmoi source-path)'
+
+function cme
+	chezmoi edit $argv
 end
+
+function cma
+	chezmoi add $argv
+end
+
